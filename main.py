44425b5b423f751cab98b4ac471f02d41f19ccdc
jimmy20180130 <68285469+jimmy20180130@users.noreply.github.com>
2023-08-23 22:39:12 +0800

    add lunch_count

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from discord.ext import commands
 import os
 from discord_webhook import DiscordWebhook, DiscordEmbed
 import asyncio
+import time
+import schedule
 
 with open("settings.json", "r", encoding='utf-8') as settings:
     setting = json.load(settings)
@@ -16,6 +18,20 @@ with open("lunch.json", "r", encoding='utf-8') as lunch_file:
 # 取得user.json的資料
 with open("user.json", "r") as user_file:
     user_data = json.load(user_file)
+
+def count_lunch_items():
+    lunch_counts = {lunch_data[lunch_id]["name"]: 0 for lunch_id in lunch_data}
+
+    for user_id, data in user_data.items():
+        lunch_id = data["lunch"]
+        if lunch_id in lunch_data:
+            lunch_name = lunch_data[lunch_id]["name"]
+            lunch_counts[lunch_name] += 1
+
+    lunch_counts_list = [(count, lunch_name) for lunch_name, count in lunch_counts.items()]
+    lunch_counts_list.sort(reverse=True)  # Sort by count in descending order
+
+    return lunch_counts_list
 
 intents = discord.Intents().all()
 bot = commands.Bot(command_prefix=setting['prefix'], intents=intents, help_command=None)
@@ -121,6 +137,17 @@ async def add_discord(ctx, user_id :int, discord_id :int):
     with open('user.json', 'w', encoding='utf-8') as user_file:
         json.dump(user_data, user_file, ensure_ascii=False, indent=4)
     await ctx.send(f'{user_id}號的帳號已與<@{discord_id}>連結')
+
+@bot.command()
+@commands.is_owner()
+async def count_lunch(ctx):
+    webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1143563857797849260/xCaTsRsc7SGOv1PejwjD-yZDSqMQnKBzGOQ_hKrndZl05J396h53nf6ooSAJNSvuir_G")
+    embed = DiscordEmbed(title="午餐統計", color=0x03b2f8)
+    for count, lunch_name in count_lunch_items():
+        embed.add_embed_field(name=lunch_name, value=str(count), inline=False)
+    # add embed object to webhook
+    webhook.add_embed(embed)
+    response = webhook.execute()
 
 def load_sheet():
     # 認證+讀取google sheet
@@ -279,12 +306,22 @@ def process_orders(orders):
                 webhook.add_embed(embed)
                 response = webhook.execute()
 
+def start_scheduler():
+    # Schedule the count_lunch_items function to run daily at 4:00 PM
+    schedule.every().day.at("16:00").do(count_lunch_items)
+
+    # Run the scheduled tasks
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
 async def start_timer():
     while True:
-        await asyncio.sleep(600)  # 等待 600 秒（10 分鐘）
         now_data = load_sheet()
         if now_data != ([] or 'no data'):
             process_orders(now_data)
+        await asyncio.sleep(600)  # 等待 600 秒（10 分鐘）
 
 if __name__ == "__main__":
     bot.run('MTE0MzU0ODA5NjczNTg3OTI5OQ.G-bu59.2-JypjPXLv6ubR4ldLJupEHC4a241HZSRe5sto')
+    start_scheduler()
